23acdd63328e24f809cca6fd0a39826f4fd63372
”Ozi <31310132+OziOcb@users.noreply.github.com>
2018-04-07 21:36:32 +0100

    Give a solution to the 3rd project

--- assets/js/003.js
+++ assets/js/003.js
@@ -1,1 +1,9 @@
-console.log('elo');
+function generateRange(min, max, step) {
+  let arr = [];
+  for (let i = min; i <= max; i = i + step) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+console.log(generateRange(2, 10, 2));
